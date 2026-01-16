0162e2eebe3735b9146db935a5ffe7697f572c71
JohnW44 <johnwintz@gmail.com>
2026-01-16 13:52:34 -0800

    added more debugging

--- NoWorries/start.sh
+++ NoWorries/start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+export SECRET_KEY
+export DATABASE_URL
+export FLASK_APP
+export FLASK_ENV
+export SCHEMA
+
 echo "DEBUG: SECRET_KEY is set: $(if [ -n \"$SECRET_KEY\" ]; then echo 'YES'; else echo 'NO'; fi)"
-echo "DEBUG: DATABASE_URL is set: $(if [ -n \"$DATABASE_URL\" ]; then echo 'YES'; else echo 'NO'; fi)"
+echo "DEBUG from Python:"
+python -c "import os; print('SECRET_KEY:', 'SET' if os.environ.get('SECRET_KEY') else 'NOT SET')"
+
 flask db upgrade
 flask seed all
 exec gunicorn app:app
